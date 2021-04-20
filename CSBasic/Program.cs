b3d43614702dcf23b43786fbfdb1cea7789d6162
Park-Ji-Yoon <purun030515@gmail.com>
2021-04-20 12:18:50 +0900

    #8 C# 기본 자료형 실습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -39,6 +39,42 @@ namespace CSBasic
             Console.WriteLine("");
             Console.WriteLine("이건 개행");
 
+            // 자료형 (Data Type)
+            // Integer
+            Console.WriteLine(53 + 272 - 12);
+            Console.WriteLine(3 * 8 * 2);
+            // C#에서는 정수 나눗셈을 하면 정수로 나옴
+            Console.WriteLine(14 / 2); // 7
+            Console.WriteLine(14 / 3); // 4
+            Console.WriteLine(14 % 2);
+
+            // Real Number
+            Console.WriteLine(13.123);
+            Console.WriteLine(0);
+            Console.WriteLine(0.0);
+            // C#에서는 실수 나눗셈을 하면 실수로 나옴
+            Console.WriteLine(14.0 / 2.0); // 7.0
+            Console.WriteLine(14.0 / 3.0); // 4.6666667
+            // Console.WriteLine(14.0 % 3.0);
+            Console.WriteLine(14 / 3); // 4
+            Console.WriteLine(14.0 / 3); // 4.6666667
+            Console.WriteLine(14 / 3.0); // 4.6666667
+            Console.WriteLine(14.0 / 2.0); // 4.6666667
+
+            // Character
+            Console.WriteLine('A'); // character (문자)
+            // String
+            Console.WriteLine("A"); // string (문자열)
+            // Escape Character
+            Console.WriteLine("\t 탬 \\ 역슬래시 \n 개행 \" 쌍따옴표");
+            // 앞글자를 포함해서 8byte
+            Console.WriteLine("미\t림여자정보과학고");
+            Console.WriteLine("미림\t여자정보과학고");
+            Console.WriteLine("미림여\t자정보과학고");
+            Console.WriteLine("미림여자\t정보과학고");
+            Console.WriteLine("미림여자정\t보과학고");
+            Console.WriteLine("미림여자정보\t과학고");
+            Console.WriteLine("미림여자정보과\t학고");
         }
     }
 }
