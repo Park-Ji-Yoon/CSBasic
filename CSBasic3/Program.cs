1c082800064b25304d565cc1df52eda65946051a
s2019s12 <s2019s12@e-mirim.hs.kr>
2021-05-13 08:51:23 +0900

    #18 for / 역 for문 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -13,7 +13,7 @@ namespace CSBasic3
             // Console.WriteLine(intArray[-]); // 음수 사용 불가
             int i = 0;
             int[] intArray2 = { 52, 273, 32, 65, 103 };
-            while (i < intArray.Length)
+            while (i < intArray2.Length)
             {
                 Console.WriteLine(i + "번째 출력" + intArray2[i]);
                 i++;
@@ -24,9 +24,19 @@ namespace CSBasic3
                 Console.Write("입력(종료:exit):");
                 input = Console.ReadLine();
             } while (input != "exit");
-            for (int j = '가'; j <= '힣'; j++)
+            /*for (int j = '가'; j <= '힣'; j++)
             {
                 Console.WriteLine((char)j);
+            }*/
+
+            int[] intArray3 = { 1, 2, 3, 4, 5, 6 };
+            for (int k = intArray3.Length - 1; k >= 0; k--)
+            {
+                Console.WriteLine(intArray3[k]);
+            }
+            for (int k=intArray3.Length; k>0; k--)
+            {
+                Console.WriteLine(intArray3[k-1]);
             }
         }
     }
