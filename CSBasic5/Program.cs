ef4e3784ab78f5d687d0f19f667bdac7b896747b
Park-Ji-Yoon <purun030515@gmail.com>
2021-06-22 12:28:50 +0900

    #28 메서드 기본 형태 , 매개변수 (Power, Multi, Print)

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,11 +8,34 @@ namespace CSBasic5
 {
     class Program
     {
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x * x;
+            }
+
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출됨");
+            }
+        }
+
+        
+
         static void Main(string[] args)
         {
-            Car car = new Car();
-            car.SetInTime();
-            car.SetOutTime();
+            MethodExample me = new MethodExample();
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
+            Console.WriteLine(me.Multi(2123, 275));
+            Console.WriteLine(me.Multi(100, 3210));
+            me.Print();
         }
     }
 }
