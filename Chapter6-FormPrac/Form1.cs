e0817d59380180bf55dbdbae597a8d5c4ce4bf8d
Park-Ji-Yoon <purun030515@gmail.com>
2021-08-26 10:10:36 +0900

    MDI 적용 실습

--- Chapter6-FormPrac/Form1.cs
+++ Chapter6-FormPrac/Form1.cs
@@ -15,31 +15,31 @@ namespace Chapter6_FormPrac
         public Form1()
         {
             InitializeComponent();
+            IsMdiContainer = true;
         }
 
         private void btnMessageBox1_Click(object sender, EventArgs e)
         {
             DialogResult result = DialogResult.None;
+
             if (sender == btnMessageBox1)
             {
                 result = MessageBox.Show("안녕하세요?");
-            }else if(sender == btnMessageBox2)
+            }
+            else if (sender == btnMessageBox2)
             {
                 result = MessageBox.Show("안녕하세요?", "환영인사");
             }
             else if (sender == btnMessageBox3)
             {
-                result = MessageBox.Show("안녕하세요?", "환영인사",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Question
-
-                    );
+                result = MessageBox.Show("안녕하세요?", "환영인사", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             }
 
             switch (result)
             {
                 case DialogResult.OK:
                 case DialogResult.Yes:
-                    MessageBox.Show("좋은 하루~", "시스템");
+                    MessageBox.Show("좋은 하루~!", "시스템");
                     break;
                 case DialogResult.No:
                     MessageBox.Show("가서 쉬세요!", "시스템");
@@ -50,10 +50,13 @@ namespace Chapter6_FormPrac
         private void btnModeless_Click(object sender, EventArgs e)
         {
             Form form = new FormCustom();
-            if(sender == btnModeless)
+
+            if (sender == btnModeless)
             {
+                form.MdiParent = this;
                 form.Show();
-            }else if(sender == btnModal)
+            }
+            else if (sender == btnModal)
             {
                 form.ShowDialog();
             }
