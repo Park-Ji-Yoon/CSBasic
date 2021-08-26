5491cbfc926ae3dd1302f21d47bffe05b29304a5
Park-Ji-Yoon <purun030515@gmail.com>
2021-08-26 10:03:56 +0900

    모달과 모달리스 실습

--- Chapter6-FormPrac/Form1.Designer.cs
+++ Chapter6-FormPrac/Form1.Designer.cs
@@ -29,13 +29,85 @@ namespace Chapter6_FormPrac
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMessageBox2 = new System.Windows.Forms.Button();
+            this.btnMessageBox3 = new System.Windows.Forms.Button();
+            this.btnMessageBox1 = new System.Windows.Forms.Button();
+            this.btnModeless = new System.Windows.Forms.Button();
+            this.btnModal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox2
+            // 
+            this.btnMessageBox2.Location = new System.Drawing.Point(167, 28);
+            this.btnMessageBox2.Name = "btnMessageBox2";
+            this.btnMessageBox2.Size = new System.Drawing.Size(85, 91);
+            this.btnMessageBox2.TabIndex = 1;
+            this.btnMessageBox2.Text = "메세지   박스 보기";
+            this.btnMessageBox2.UseVisualStyleBackColor = true;
+            this.btnMessageBox2.Click += new System.EventHandler(this.btnMessageBox1_Click);
+            // 
+            // btnMessageBox3
+            // 
+            this.btnMessageBox3.Location = new System.Drawing.Point(278, 28);
+            this.btnMessageBox3.Name = "btnMessageBox3";
+            this.btnMessageBox3.Size = new System.Drawing.Size(85, 91);
+            this.btnMessageBox3.TabIndex = 2;
+            this.btnMessageBox3.Text = "메세지   박스 보기";
+            this.btnMessageBox3.UseVisualStyleBackColor = true;
+            this.btnMessageBox3.Click += new System.EventHandler(this.btnMessageBox1_Click);
+            // 
+            // btnMessageBox1
+            // 
+            this.btnMessageBox1.Location = new System.Drawing.Point(51, 28);
+            this.btnMessageBox1.Name = "btnMessageBox1";
+            this.btnMessageBox1.Size = new System.Drawing.Size(85, 91);
+            this.btnMessageBox1.TabIndex = 3;
+            this.btnMessageBox1.Text = "메세지   박스 보기";
+            this.btnMessageBox1.UseVisualStyleBackColor = true;
+            this.btnMessageBox1.Click += new System.EventHandler(this.btnMessageBox1_Click);
+            // 
+            // btnModeless
+            // 
+            this.btnModeless.Location = new System.Drawing.Point(51, 153);
+            this.btnModeless.Name = "btnModeless";
+            this.btnModeless.Size = new System.Drawing.Size(85, 91);
+            this.btnModeless.TabIndex = 4;
+            this.btnModeless.Text = "모달리스 창 열기";
+            this.btnModeless.UseVisualStyleBackColor = true;
+            this.btnModeless.Click += new System.EventHandler(this.btnModeless_Click);
+            // 
+            // btnModal
+            // 
+            this.btnModal.Location = new System.Drawing.Point(167, 153);
+            this.btnModal.Name = "btnModal";
+            this.btnModal.Size = new System.Drawing.Size(85, 91);
+            this.btnModal.TabIndex = 5;
+            this.btnModal.Text = "모달 창 열기";
+            this.btnModal.UseVisualStyleBackColor = true;
+            this.btnModal.Click += new System.EventHandler(this.btnModeless_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(421, 276);
+            this.Controls.Add(this.btnModal);
+            this.Controls.Add(this.btnModeless);
+            this.Controls.Add(this.btnMessageBox1);
+            this.Controls.Add(this.btnMessageBox3);
+            this.Controls.Add(this.btnMessageBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnMessageBox2;
+        private System.Windows.Forms.Button btnMessageBox3;
+        private System.Windows.Forms.Button btnMessageBox1;
+        private System.Windows.Forms.Button btnModeless;
+        private System.Windows.Forms.Button btnModal;
     }
 }
 
