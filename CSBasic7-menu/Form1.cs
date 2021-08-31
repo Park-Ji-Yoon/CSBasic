71082381d127ecfee56c3535e51a6dc33a3f0033
Park-Ji-Yoon <purun030515@gmail.com>
2021-08-31 13:10:07 +0900

    menustrip, statusstrip 실습 예제 (Winform)

--- CSBasic7-menu/Form1.cs
+++ CSBasic7-menu/Form1.cs
@@ -16,5 +16,39 @@ namespace CSBasic7_menu
         {
             InitializeComponent();
         }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 상태표시줄ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            상태표시줄ToolStripMenuItem.Checked = !상태표시줄ToolStripMenuItem.Checked;
+            toolStrip1.Visible = 상태표시줄ToolStripMenuItem.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(progressBar2.Value + 10 < progressBar2.Maximum)
+            {
+                progressBar2.Value += 10;
+            }
+            else
+            {
+                progressBar2.Value = progressBar2.Maximum;
+            }
+            
+        }
+
+        private void 메모장정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fake 메모장!!", "안내", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
