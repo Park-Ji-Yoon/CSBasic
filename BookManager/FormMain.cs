6c3dd6601c942ca39817e71a5134473f9d6b19fa
Park-Ji-Yoon <purun030515@gmail.com>
2021-09-27 15:10:38 +0900

    FormMain에 Labe, Button 추가

--- BookManager/FormMain.cs
+++ BookManager/FormMain.cs
@@ -15,7 +15,21 @@ namespace BookManager
         public Form1()
         {
             InitializeComponent();
-            DataManager d = new DataManager();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
